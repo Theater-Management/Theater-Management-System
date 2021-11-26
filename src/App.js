2f5876edfa635714d94599e19c5e2e90a3976340
A-Chathumini <ackatuwawala@gmail.com>
2021-11-26 20:55:50 +0530

    Create theatre signup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MovieList from "./Movie/MovieList";
 import Footer from "./OtherPages/Footer";
 import NavBar from "./OtherPages/NavBar";
 import SignUp from "./Sign Up/SignUp";
+import TheatreSignUp from "./admin/TheatreSignUp";
 
 const App = () => {
   return (
@@ -17,6 +18,9 @@ const App = () => {
         <Route exact path="/signup">
           <SignUp />
         </Route>
+          <Route exact path="/theatresignup">
+            <TheatreSignUp />
+          </Route>
         <Route exact path="/movie-list">
           <MovieList />
         </Route>
---
+++ src/admin/TheatreSignUp.js
@@ -0,0 +1,186 @@
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+//firebase
+import {auth, db} from "../firebase/firebase";
+import {makeStyles} from "@material-ui/core/styles";
+import {createUserWithEmailAndPassword} from "firebase/auth";
+import {doc, setDoc} from "firebase/firestore";
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Avatar from "@material-ui/core/Avatar";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Select from "@material-ui/core/Select";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
+// import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: "100%", // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+const TheatreSignUp = () => {
+
+    const history = useHistory();
+    const classes = useStyles();
+
+    const [details, setDetails] = useState({
+        theatrename: "",
+        location: "",
+        capacity: "",
+        email: "",
+        password: "",
+        type: "",
+    });
+
+    const setValue = (e) =>
+        setDetails((details) => ({ ...details, [e.target.name]: e.target.value }));
+
+
+    const handleSubmit = () => {
+        console.log(details);
+
+        console.log(details);
+        //===============================
+        createUserWithEmailAndPassword(auth, details.email, details.password)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+
+                setDoc(doc(db, "users", user.uid), {
+                    theatrename: details.theatrename,
+                    location: details.location,
+                    capacity: details.capacity,
+                    email: details.email,
+                    password: details.password,
+                    type: "theatre",
+                });
+
+                console.log("created");
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+            });
+
+        //==============================
+        history.push("movie-list");
+        //------------------------------------------------------
+    };
+
+    return (
+        <Container style={{ height: "100vh" }} maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                   Theatre Sign up
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                autoComplete="theatrename"
+                                name="theatrename"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="theatrename"
+                                label="Threatre Name"
+                                value={details.theatrename}
+                                autoFocus
+                                onChange={setValue}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="location"
+                                label="Location"
+                                name="location"
+                                autoComplete="location"
+                                value={details.location}
+                                onChange={setValue}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="capacity"
+                                label="capacity"
+                                name="capacity"
+                                autoComplete="capacity"
+                                value={details.capacity}
+                                onChange={setValue}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                value={details.email}
+                                autoComplete="email"
+                                onChange={setValue}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                value={details.password}
+                                autoComplete="current-password"
+                                onChange={setValue}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={handleSubmit}
+                    >
+                        Sign Up
+                    </Button>
+                    <Grid container justifyContent="flex-end">
+                    </Grid>
+                </form>
+            </div>
+        </Container>
+    );
+};
+export default TheatreSignUp;
