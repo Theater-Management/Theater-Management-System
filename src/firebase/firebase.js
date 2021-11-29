69732050050fdbbe4cb6360dc8c78033f561f9a6
Nimasha Bandara <nimashak98@gmail.com>
2021-11-29 15:16:22 +0530

    previousdatabase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,31 +3,32 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAxSiUGfiKx9qSEHWFMtLXaqbEls3QCsHQ",
-//   authDomain: "theater-management-system.firebaseapp.com",
-//   projectId: "theater-management-system",
-//   storageBucket: "theater-management-system.appspot.com",
-//   messagingSenderId: "415401391425",
-//   appId: "1:415401391425:web:32eb807105dda0a7405dc0",
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyAxSiUGfiKx9qSEHWFMtLXaqbEls3QCsHQ",
+  authDomain: "theater-management-system.firebaseapp.com",
+  projectId: "theater-management-system",
+  storageBucket: "theater-management-system.appspot.com",
+  messagingSenderId: "415401391425",
+  appId: "1:415401391425:web:32eb807105dda0a7405dc0",
+};
 
 // // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const db = getFirestore(app);
-// export const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
 
-// export default app;
+export default app;
 
+//=============================================================================================
+/*
 //testing database
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyD8lJayckj8dgRTtgAhYUaoDHlGkuU5D1w",
   authDomain: "testing-c4088.firebaseapp.com",
@@ -42,3 +43,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+*/
